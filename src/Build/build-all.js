2a633bd0fe823a106635cd979217c83930be29e5
bgseong <qnrms2898@naver.com>
2023-02-24 17:00:58 +0900

    아이템 아이콘 추가

--- src/Build/build-all.js
+++ src/Build/build-all.js
@@ -4,6 +4,11 @@ import './build-all.css';
 import Comet from '../img/comet.png';
 import Uit from '../img/uit.png';
 
+import Hydra from '../img/hydra.png';
+import Moon from '../img/moon.png';
+import Selida from '../img/selida.png';
+
+
 import Detail from './detail';
 
 const Build_all= (props) => {
@@ -20,7 +25,11 @@ const Build_all= (props) => {
             <img src={Comet} alt="유성"></img>
             <img src={Uit} alt="영감"></img>
             </div>
-        <div className="items"></div>
+        <div className="items">
+            <img src={Hydra} alt=""></img>
+            <img src={Moon} alt=""></img>
+            <img src={Selida} alt=""></img>
+        </div>
         </div>
         <Detail hidden={hid}/>     
       </>
